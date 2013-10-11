35e39c95034a03b6ad95ec5a04cbd35128af73b1
Chris Malley <cmalley@pixelzoom.com>
2013-10-11 11:46:01 -0600

    fix lint issue in GameTimer

--- js/GameTimer.js
+++ js/GameTimer.js
@@ -30,14 +30,14 @@ define( function( require ) {
 
   /**
    * Formats a value representing seconds into H:MM:SS (localized).
-   * @param {Number} seconds
+   * @param {Number} time in seconds
    * @returns {string}
    */
-  GameTimer.formatTime = function( seconds ) {
+  GameTimer.formatTime = function( time ) {
 
-    var hours = Math.floor( seconds / 3600 );
-    var minutes = Math.floor( (seconds - (hours * 3600)) / 60 );
-    var seconds = Math.floor( seconds - (hours * 3600) - (minutes * 60) );
+    var hours = Math.floor( time / 3600 );
+    var minutes = Math.floor( (time - (hours * 3600)) / 60 );
+    var seconds = Math.floor( time - (hours * 3600) - (minutes * 60) );
 
     var minutesString = ( minutes > 9 ) ? minutes.toString() : ( '0' + minutes );
     var secondsString = ( seconds > 9 ) ? seconds.toString() : ( '0' + seconds );
