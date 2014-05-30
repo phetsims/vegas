79a79ba28d6b1bfc9298db3a6c6c081b0e3a1577
Sam Reid <reids@colorado.edu>
2014-05-30 10:17:10 -0600

    converted to use CanvasNode, see #4

--- js/RewardNode.js
+++ js/RewardNode.js
@@ -6,10 +6,9 @@
  * Notes:
  * 1. Instead of continuous animation, the animation is shown as a transient behavior that shows for a few seconds and then disappears.
  *    This is to encourage the student to move to the next challenge, instead of becoming mesmerized by a perpetual animation.
- * 2. We could make the performance optimization to only animate objects that are currently onscreen, but that would lead to slowing down and speeding up behavior
- * 3. TODO: I would like to add optional support for rotation, scaling or other visual candy
- * 4. TODO: It is not exactly right to use the layout bounds, we should instead use the visible screen bounds.  EnergySkatePark has an example of computing the visual screen bounds.
- * 5. TODO: Provide utility support for the default node creation code and client code so that nodes can automatically be replicated and cached.
+ * 2. TODO: I would like to add optional support for rotation, scaling or other visual candy
+ * 3. TODO: It is not exactly right to use the layout bounds, we should instead use the visible screen bounds.  EnergySkatePark has an example of computing the visual screen bounds.
+ * 4. TODO: Provide utility support for the default node creation code and client code so that nodes can automatically be replicated and cached.
  *
  * @author Sam Reid
  */
@@ -19,75 +18,82 @@ define( function( require ) {
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
+  var CanvasNode = require( 'SCENERY/nodes/CanvasNode' );
   var ScreenView = require( 'JOIST/ScreenView' );
   var StarNode = require( 'SCENERY_PHET/StarNode' );
   var FaceNode = require( 'SCENERY_PHET/FaceNode' );
   var Image = require( 'SCENERY/nodes/Image' );
+  var Bounds2 = require( 'DOT/Bounds2' );
+
+  var debug = false;
 
   function RewardNode( options ) {
-    options = _.extend( {
 
-      layoutBounds: ScreenView.DEFAULT_LAYOUT_BOUNDS,
+    this.options = options = _.extend( {
 
-      //Default nodes are many stars and smiling faces.
-      //To avoid allocating many default nodes, if the defaults are not used, bury the node creation in a function
-      createNodes: function() {
+      //Bounds in which to render the canvas.  TODO: Should be full screen
+      canvasBounds: new Bounds2( 0, 0, ScreenView.DEFAULT_LAYOUT_BOUNDS.width, ScreenView.DEFAULT_LAYOUT_BOUNDS.height ),
 
-        //Buffer nodes as images for performance
-        var starNode = new Node();
-        var faceNode = new Node();
+      //Nodes to appear in the reward node.  They will be cached as images to improve performance
+      nodes: [
+        new FaceNode( 40, {headStroke: 'black', headLineWidth: 1.5} ),
+        new StarNode()
+      ],
 
-        //Create a star node
-        new StarNode().toImage( function( image ) {
-          starNode.children = [new Image( image )];
-        } );
-
-        //Create a smiling face.
-        new FaceNode( 40, {headStroke: 'black', headLineWidth: 1.5} ).toImage( function( image ) {
-          faceNode.children = [new Image( image )];
-        } );
-
-        var stars = _.times( 50, function() {
-          return new Node( {children: [starNode]} );
-        } );
-        var faces = _.times( 50, function() {
-          return new Node( {children: [faceNode]} );
-        } );
-        return _.shuffle( stars.concat( faces ) );
-      }
+      //Total number of nodes to display
+      rewardNodeCount: 100
     }, options );
 
+    //Cache the nodes as images.  Use an intermediate imageWrapper since the images will be returned later asynchronously
+    //And we need a place to store them, and know when they have arrived
+    var imageWrappers = [];
+    options.nodes.forEach( function( node, i ) {
+      imageWrappers.push( {image: null} );
+      node.toImage( function( image ) {
+        imageWrappers[i].image = image;
+      } );
+    } );
 
-    var children = options.createNodes();
-    var layoutBounds = options.layoutBounds;
-
-    this.speeds = [];
-
-    //randomly scatter children above the top of the screen
-    for ( var i = 0; i < children.length; i++ ) {
-      var child = children[i];
-
-      //Hack alert!  Would be nice to use the node bounds here, but if they are created using toImage we may not have the bounds yet.  So use x/y as a workaround
-      //Hack alert 2.0! Since the nodes may not have finished toImage()ing yet, move them up some extra pixels so they will be finished caching before they get to the screen
-      child.y = layoutBounds.top - Math.random() * layoutBounds.height - 200;
-      child.x = Math.random() * layoutBounds.width + layoutBounds.left;
-
-      this.speeds.push( Math.random() + 1 );
+    //Store each reward, which has an imageWrapper (see above), x, y, speed
+    this.rewards = [];
+    for ( var i = 0; i < options.rewardNodeCount; i++ ) {
+      this.rewards.push( {
+        imageWrapper: imageWrappers[i % imageWrappers.length],
+        x: Math.random() * options.canvasBounds.width + options.canvasBounds.left,
+        y: options.canvasBounds.top - Math.random() * options.canvasBounds.height - 200,
+        speed: (Math.random() + 1) * 60
+      } );
     }
 
-    Node.call( this, {children: children} );
-
-    this.mutate( options );
+    CanvasNode.call( this, options );
   }
 
-  return inherit( Node, RewardNode, {
-    step: function( elapsed ) {
-      var children = this.children;
-      var numChildren = children.length;
-      for ( var i = 0; i < numChildren; i++ ) {
-        var child = children[i];
-        child.translate( 0, 1 * elapsed * 60 * this.speeds[i] );
+  return inherit( CanvasNode, RewardNode, {
+
+    //Paint the rewards on the canvas
+    // @param {CanvasContextWrapper} wrapper
+    paintCanvas: function( wrapper ) {
+      var context = wrapper.context;
+      if ( debug ) {
+        context.fillStyle = 'rgba(50,50,50,0.5)';
+        context.fillRect( 0, 0, this.options.canvasBounds.width, this.options.canvasBounds.height );
       }
+
+      for ( var i = 0; i < this.rewards.length; i++ ) {
+        var reward = this.rewards[i];
+        if ( reward.imageWrapper.image ) {
+          context.drawImage( reward.imageWrapper.image, reward.x, reward.y );
+        }
+      }
+    },
+
+    //Move the rewards down according to their speed
+    step: function( dt ) {
+      for ( var i = 0; i < this.rewards.length; i++ ) {
+        var reward = this.rewards[i];
+        reward.y += reward.speed * dt;
+      }
+      this.invalidatePaint();
     }
   } );
 } );
