599ffbcdeccc726de29473099f76e5a6446c122f
Sam Reid <reids@colorado.edu>
2014-05-29 23:30:45 -0600

    Moved node creation to a function to avoid creating the defaults if the defaults are not used, see #4

--- js/RewardNode.js
+++ js/RewardNode.js
@@ -7,7 +7,8 @@
  * 1. Instead of continuous animation, the animation is shown as a transient behavior that shows for a few seconds and then disappears.
  *    This is to encourage the student to move to the next challenge, instead of becoming mesmerized by a perpetual animation.
  * 2. We could make the performance optimization to only animate objects that are currently onscreen, but that would lead to slowing down and speeding up behavior
- * 3. I would like to add optional support for rotation
+ * 3. I would like to add optional support for rotation, scaling or other visual candy
+ * 4. It is not exactly right to use the layout bounds, we should instead use the visible screen bounds.  EnergySkatePark has an example of computing the visual screen bounds.
  *
  * @author Sam Reid
  */
@@ -22,34 +23,41 @@ define( function( require ) {
   var FaceNode = require( 'SCENERY_PHET/FaceNode' );
   var Image = require( 'SCENERY/nodes/Image' );
 
-  var starNode = new Node();
-  var faceNode = new Node();
-
-  new StarNode().toImage( function( image ) {
-    starNode.children = [new Image( image )];
-  } );
-
-  //Create a smiling face.
-  new FaceNode( 40, {headStroke: 'black', headLineWidth: 1.5} ).toImage( function( image ) {
-    faceNode.children = [new Image( image )];
-  } );
-
   function RewardNode( options ) {
     options = _.extend( {
 
       layoutBounds: ScreenView.DEFAULT_LAYOUT_BOUNDS,
 
-      //Default nodes are many stars and smiling faces
-      nodes: _.shuffle( _.times( 100, function() {
+      //Default nodes are many stars and smiling faces.
+      //To avoid allocating many default nodes, if the defaults are not used, bury the node creation in a function
+      createNodes: function() {
+
+        //Buffer nodes as images for performance
+        var starNode = new Node();
+        var faceNode = new Node();
+
+        //Create a star node
+        new StarNode().toImage( function( image ) {
+          starNode.children = [new Image( image )];
+        } );
+
+        //Create a smiling face.
+        new FaceNode( 40, {headStroke: 'black', headLineWidth: 1.5} ).toImage( function( image ) {
+          faceNode.children = [new Image( image )];
+        } );
+
+        var stars = _.times( 100, function() {
           return new Node( {children: [starNode]} );
-        } ).concat( _.times( 100, function() {
+        } );
+        var faces = _.times( 100, function() {
           return new Node( {children: [faceNode]} );
-        } ) )
-      )
+        } );
+        return _.shuffle( stars.concat( faces ) );
+      }
     }, options );
 
 
-    var children = options.nodes;
+    var children = options.createNodes();
     var layoutBounds = options.layoutBounds;
 
     this.speeds = [];
@@ -57,8 +65,11 @@ define( function( require ) {
     //randomly scatter children above the top of the screen
     for ( var i = 0; i < children.length; i++ ) {
       var child = children[i];
-      child.bottom = layoutBounds.top - Math.random() * layoutBounds.height;
-      child.centerX = Math.random() * layoutBounds.width + layoutBounds.left;
+
+      //Hack alert!  Would be nice to use the node bounds here, but if they are created using toImage we may not have the bounds yet.  So use x/y as a workaround
+      //Hack alert 2.0! Since the nodes may not have finished toImage()ing yet, move them up some extra pixels so they will be finished caching before they get to the screen
+      child.y = layoutBounds.top - Math.random() * layoutBounds.height - 200;
+      child.x = Math.random() * layoutBounds.width + layoutBounds.left;
 
       this.speeds.push( Math.random() + 1 );
     }
