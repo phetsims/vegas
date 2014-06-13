d0c33fef1433c22de15c490297b5f3014e6cccca
Sam Reid <reids@colorado.edu>
2014-06-13 13:21:53 -0600

    Added createRandomNodes static method, see #4

--- js/RewardNode.js
+++ js/RewardNode.js
@@ -87,143 +87,159 @@ define( function( require ) {
 
   return inherit( CanvasNode, RewardNode, {
 
-    //Paint the rewards on the canvas
-    // @param {CanvasContextWrapper} wrapper
-    paintCanvas: function( wrapper ) {
-      var context = wrapper.context;
+      //Paint the rewards on the canvas
+      // @param {CanvasContextWrapper} wrapper
+      paintCanvas: function( wrapper ) {
+        var context = wrapper.context;
 
-      //If the debugging flag is on, show the bounds of the canvas
-      if ( debug ) {
-        var bounds = this.options.canvasBounds;
+        //If the debugging flag is on, show the bounds of the canvas
+        if ( debug ) {
+          var bounds = this.options.canvasBounds;
 
-        //Fill the canvas with gray
-        context.fillStyle = 'rgba(50,50,50,0.5)';
-        context.fillRect( bounds.minX, bounds.minY, bounds.width, bounds.height );
+          //Fill the canvas with gray
+          context.fillStyle = 'rgba(50,50,50,0.5)';
+          context.fillRect( bounds.minX, bounds.minY, bounds.width, bounds.height );
 
-        //Stroke the canvas border with blue
-        context.strokeStyle = "#0000ff";
-        context.lineWidth = 5;
-        context.strokeRect( bounds.minX, bounds.minY, bounds.width, bounds.height );
-      }
-      context.scale( 1 / this.options.scaleForResolution, 1 / this.options.scaleForResolution );
+          //Stroke the canvas border with blue
+          context.strokeStyle = "#0000ff";
+          context.lineWidth = 5;
+          context.strokeRect( bounds.minX, bounds.minY, bounds.width, bounds.height );
+        }
+        context.scale( 1 / this.options.scaleForResolution, 1 / this.options.scaleForResolution );
 
-      //Display the rewards, but check that they exist first.  They do not exist when attached to the timer with stepSource
-      if ( this.rewards ) {
+        //Display the rewards, but check that they exist first.  They do not exist when attached to the timer with stepSource
+        if ( this.rewards ) {
+          for ( var i = 0; i < this.rewards.length; i++ ) {
+            var reward = this.rewards[i];
+            if ( reward.imageWrapper.image ) {
+              context.drawImage( reward.imageWrapper.image, reward.x, reward.y );
+            }
+          }
+        }
+      },
+
+      getScene: function() {
+        //Find the root of the scene tree
+        var node = this;
+        while ( node ) {
+          assert && assert( node._parents[1] === undefined, 'globalToLocalPoint unable to work for DAG' );
+          if ( node._parents[0] ) {
+            node = node._parents[0];
+          }
+          else {
+            break;
+          }
+        }
+
+        //TODO: add assertion to make sure node is a scenery.Scene
+        return node;
+      },
+
+      //Find the first parent that is a ScreenView so we can listen for its transform, see https://github.com/phetsims/vegas/issues/4
+      getScreenView: function() {
+        var node = this;
+        while ( node ) {
+          assert && assert( node._parents[1] === undefined, 'globalToLocalPoint unable to work for DAG' );
+          if ( node._parents[0] ) {
+            node = node._parents[0];
+            if ( node instanceof ScreenView ) {
+              return node;
+            }
+          }
+          else {
+            break;
+          }
+        }
+
+        throw new Error( 'No ScreenView found' );
+      },
+
+      //Only init after being attached to the scene graph, since we must ascertain the local bounds such that they take up the global screen.
+      // 1. listen to the size of the scene/display
+      // 2. record the trail between the scene and your CanvasNode, and
+      // 3. apply the inverse of that transform to the CanvasNode (whenever an ancestor's transform changes, or when the scene/display size changes).
+      //
+      // Later @jonathanolson continued:
+      // I don't recall how Scenery will handle Bounds2.EVERYTHING (probably assertion fails) right now, but that can change
+      // for implementing now, I'd watch the iso transform, compute the inverse, and set bounds on changes to be precise (since you need them anyways to draw)
+      init: function() {
+        var rewardNode = this;
+        var scene = this.getScene();
+
+        //Listen to the bounds of the scene, so the canvas can be resized if the window is reshaped
+        var updateBounds = function() {
+
+          var local = rewardNode.globalToLocalBounds( scene.sceneBounds );
+          rewardNode.setCanvasBounds( local );
+
+          //Also, store the bounds in the options so the debug flag can render the bounds
+          rewardNode.options.canvasBounds = local;
+        };
+
+        //When the scene is resized, update the bounds
+        scene.addEventListener( 'resize', updateBounds );
+
+        //When the ScreenView transform changes, update the bounds.  This prevents a "behind by one" problem, see https://github.com/phetsims/vegas/issues/4
+        this.getScreenView().getTransform().addTransformListener( {before: function() {}, after: function() {updateBounds();}} );
+
+        //Set the initial bounds
+        updateBounds();
+
+        //Store each reward, which has an imageWrapper (see above), x, y, speed
+        this.rewards = [];
+        for ( var i = 0; i < this.options.rewardNodeCount; i++ ) {
+          var imageWrapper = this.imageWrappers[i % this.imageWrappers.length];
+          this.rewards.push( {
+            imageWrapper: imageWrapper,
+            x: (Math.random() * this.options.canvasBounds.width + this.options.canvasBounds.left) * this.options.scaleForResolution - imageWrapper.width / 2,
+            y: this.options.canvasBounds.top - Math.random() * this.options.canvasBounds.height - 200,
+            speed: (Math.random() + 1) * 200
+          } );
+        }
+      },
+
+      //Move the rewards down according to their speed
+      step: function( dt ) {
+        if ( !this.inited && this.getScene() !== null ) {
+          this.init();
+          this.inited = true;
+        }
+
+        //Update all of the rewards
+        //Track their location so that we know when the animation is complete (when all nodes are offscreen)
+        var minY = this.rewards[0].y;
         for ( var i = 0; i < this.rewards.length; i++ ) {
           var reward = this.rewards[i];
-          if ( reward.imageWrapper.image ) {
-            context.drawImage( reward.imageWrapper.image, reward.x, reward.y );
+          reward.y += reward.speed * dt;
+          minY = Math.min( minY, reward.y );
+        }
+        this.invalidatePaint();
+
+        //If the icons are all off screen, then detach the step listener and signify completion
+        if ( minY > this.options.canvasBounds.height * this.options.scaleForResolution ) {
+          if ( this.options.stepSource ) {
+            this.options.stepSource.off( 'step', this.stepCallback );
+          }
+          if ( this.options.completionListener ) {
+            this.options.completionListener();
           }
         }
       }
     },
 
-    getScene: function() {
-      //Find the root of the scene tree
-      var node = this;
-      while ( node ) {
-        assert && assert( node._parents[1] === undefined, 'globalToLocalPoint unable to work for DAG' );
-        if ( node._parents[0] ) {
-          node = node._parents[0];
+    //Static methods
+    {
+      /**
+       * Convenience factory method to create an array of the specified nodes in an even distribution.
+       * @param {Array[Node]} nodes array of nodes to use
+       * @param {Number} count
+       */
+      createRandomNodes: function( nodes, count ) {
+        var array = [];
+        for ( var i = 0; i < count; i++ ) {
+          array.push( nodes[i % nodes.length] );
         }
-        else {
-          break;
-        }
+        return array;
       }
-
-      //TODO: add assertion to make sure node is a scenery.Scene
-      return node;
-    },
-
-    //Find the first parent that is a ScreenView so we can listen for its transform, see https://github.com/phetsims/vegas/issues/4
-    getScreenView: function() {
-      var node = this;
-      while ( node ) {
-        assert && assert( node._parents[1] === undefined, 'globalToLocalPoint unable to work for DAG' );
-        if ( node._parents[0] ) {
-          node = node._parents[0];
-          if ( node instanceof ScreenView ) {
-            return node;
-          }
-        }
-        else {
-          break;
-        }
-      }
-
-      throw new Error( 'No ScreenView found' );
-    },
-
-    //Only init after being attached to the scene graph, since we must ascertain the local bounds such that they take up the global screen.
-    // 1. listen to the size of the scene/display
-    // 2. record the trail between the scene and your CanvasNode, and
-    // 3. apply the inverse of that transform to the CanvasNode (whenever an ancestor's transform changes, or when the scene/display size changes).
-    //
-    // Later @jonathanolson continued:
-    // I don't recall how Scenery will handle Bounds2.EVERYTHING (probably assertion fails) right now, but that can change
-    // for implementing now, I'd watch the iso transform, compute the inverse, and set bounds on changes to be precise (since you need them anyways to draw)
-    init: function() {
-      var rewardNode = this;
-      var scene = this.getScene();
-
-      //Listen to the bounds of the scene, so the canvas can be resized if the window is reshaped
-      var updateBounds = function() {
-
-        var local = rewardNode.globalToLocalBounds( scene.sceneBounds );
-        rewardNode.setCanvasBounds( local );
-
-        //Also, store the bounds in the options so the debug flag can render the bounds
-        rewardNode.options.canvasBounds = local;
-      };
-
-      //When the scene is resized, update the bounds
-      scene.addEventListener( 'resize', updateBounds );
-
-      //When the ScreenView transform changes, update the bounds.  This prevents a "behind by one" problem, see https://github.com/phetsims/vegas/issues/4
-      this.getScreenView().getTransform().addTransformListener( {before: function() {}, after: function() {updateBounds();}} );
-
-      //Set the initial bounds
-      updateBounds();
-
-      //Store each reward, which has an imageWrapper (see above), x, y, speed
-      this.rewards = [];
-      for ( var i = 0; i < this.options.rewardNodeCount; i++ ) {
-        var imageWrapper = this.imageWrappers[i % this.imageWrappers.length];
-        this.rewards.push( {
-          imageWrapper: imageWrapper,
-          x: (Math.random() * this.options.canvasBounds.width + this.options.canvasBounds.left) * this.options.scaleForResolution - imageWrapper.width / 2,
-          y: this.options.canvasBounds.top - Math.random() * this.options.canvasBounds.height - 200,
-          speed: (Math.random() + 1) * 200
-        } );
-      }
-    },
-
-    //Move the rewards down according to their speed
-    step: function( dt ) {
-      if ( !this.inited && this.getScene() !== null ) {
-        this.init();
-        this.inited = true;
-      }
-
-      //Update all of the rewards
-      //Track their location so that we know when the animation is complete (when all nodes are offscreen)
-      var minY = this.rewards[0].y;
-      for ( var i = 0; i < this.rewards.length; i++ ) {
-        var reward = this.rewards[i];
-        reward.y += reward.speed * dt;
-        minY = Math.min( minY, reward.y );
-      }
-      this.invalidatePaint();
-
-      //If the icons are all off screen, then detach the step listener and signify completion
-      if ( minY > this.options.canvasBounds.height * this.options.scaleForResolution ) {
-        if ( this.options.stepSource ) {
-          this.options.stepSource.off( 'step', this.stepCallback );
-        }
-        if ( this.options.completionListener ) {
-          this.options.completionListener();
-        }
-      }
-    }
-  } );
+    } );
 } );
