d99805a123a959a1c39a5cff2f0b82f7217d9410
samreid <reids@colorado.edu>
2015-11-06 20:50:43 -0700

    updated copyright info using grunt update-copyright-dates, see  https://github.com/phetsims/chipper/issues/403

--- js/GameTimer.js
+++ js/GameTimer.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2013, University of Colorado Boulder
+// Copyright 2013-2015, University of Colorado Boulder
 
 /**
  * Game timer, keeps track of the elapsed time in the game using "wall clock" time.
