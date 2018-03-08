a0f0935603c0761ac8421680f83ee81562a36bee
Chris Malley <cmalley@pixelzoom.com>
2018-03-08 10:13:56 -0700

    update copyright date

--- js/demo/VegasScreenView.js
+++ js/demo/VegasScreenView.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2017, University of Colorado Boulder
+// Copyright 2014-2018, University of Colorado Boulder
 
 /**
  * Main ScreenView container for Buttons portion of the UI component demo.
