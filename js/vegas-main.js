ddb197424423a8272ad8396007fdc0598b240e43
Chris Malley <cmalley@pixelzoom.com>
2018-04-09 09:57:38 -0600

    delete unnecessary custom screen icons, #59
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/vegas-main.js
+++ js/vegas-main.js
@@ -10,7 +10,6 @@ define( function( require ) {
 
   // modules
   var Property = require( 'AXON/Property' );
-  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var RewardScreenView = require( 'VEGAS/demo/RewardScreenView' );
   var Screen = require( 'JOIST/Screen' );
   var Sim = require( 'JOIST/Sim' );
@@ -26,33 +25,22 @@ define( function( require ) {
     }
   };
 
-  // Creates a rectangle filled with a specified color
-  var createScreenIcon = function( color ) {
-    return new Rectangle( 0, 0, Screen.MINIMUM_HOME_SCREEN_ICON_SIZE.width, Screen.MINIMUM_HOME_SCREEN_ICON_SIZE.height, { fill: color } );
-  };
-
   SimLauncher.launch( function() {
     new Sim( vegasTitleString, [
 
       new Screen(
         function() {return {};},
-        function( model ) {return new VegasScreenView();},
-        {
+        function( model ) { return new VegasScreenView(); }, {
           name: 'Vegas',
-          backgroundColorProperty: new Property( '#fff' ),
-          homeScreenIcon: createScreenIcon( 'yellow' )
-        }
-      ),
+          backgroundColorProperty: new Property( '#fff' )
+        } ),
 
       new Screen(
         function() {return {};},
-        function( model ) {return new RewardScreenView();},
-        {
+        function( model ) { return new RewardScreenView(); }, {
           name: 'Reward',
-          backgroundColorProperty: new Property( '#fff' ),
-          homeScreenIcon: createScreenIcon( 'blue' )
-        }
-      )
+          backgroundColorProperty: new Property( '#fff' )
+        } )
     ], simOptions ).start();
   } );
 } );
