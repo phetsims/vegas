dccbaa417fdc7eb50f7f6765fef750f6f49734da
Chris Malley <cmalley@pixelzoom.com>
2018-04-09 09:21:57 -0600

    delete extra comma
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/demo/VegasScreenView.js
+++ js/demo/VegasScreenView.js
@@ -100,7 +100,7 @@ define( function( require ) {
     var levelSelectionNodeDiscreteStars = LevelSelectionItemNode.createWithScoreDisplayDiscreteStars( new Text( 'icon' ), scoreProperty, {
       scoreDisplayOptions: {
         numStars: NUM_STARS,
-        perfectScore: PERFECT_SCORE,
+        perfectScore: PERFECT_SCORE
       },
       listener: function() {
          console.log( 'level start' );
