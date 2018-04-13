f71687276e38b3b46dd7cc3b16b3ac009443a9c7
Chris Malley <cmalley@pixelzoom.com>
2018-04-13 13:03:47 -0600

    add ScoreboardBar to demo #66
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/demo/FiniteChallengesScreenView.js
+++ js/demo/FiniteChallengesScreenView.js
@@ -4,15 +4,18 @@
  * Demonstrates vegas UI components.
  *
  * @author Sam Reid
- * @author Andrea Lin
  * @author Chris Malley (PixelZoom, Inc.)
  */
 define( function( require ) {
   'use strict';
 
   // modules
+  var BooleanProperty = require( 'AXON/BooleanProperty' );
   var inherit = require( 'PHET_CORE/inherit' );
   var LevelCompletedNode = require( 'VEGAS/LevelCompletedNode' );
+  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
+  var Property = require( 'AXON/Property' );
+  var ScoreboardBar = require( 'VEGAS/ScoreboardBar' );
   var ScreenView = require( 'JOIST/ScreenView' );
   var vegas = require( 'VEGAS/vegas' );
 
@@ -24,7 +27,24 @@ define( function( require ) {
     var self = this;
     ScreenView.call( this );
 
-    //TODO add ScoreboardBar
+    // bar across the top
+    var scoreboardNode = new ScoreboardBar(
+      this.layoutBounds.width,
+      new Property( 0 ), // challenge index
+      new Property( 6 ), // challenges per level
+      new Property( 0 ), // level
+      new Property( 0 ), // score
+      new Property( 0 ), // elapse time
+      new BooleanProperty( true ), // timer enabled
+      function() { console.log( 'Start Over' ); }, // callback for 'Start Over' button
+      {
+        font: new PhetFont( 20 ),
+        leftMargin: 40, // visually aligned with left edge of challenge boxes
+        rightMargin: 50, // visually aligned with right edge of challenge graph
+        centerX: this.layoutBounds.centerX,
+        top: this.layoutBounds.top
+      } );
+    this.addChild( scoreboardNode );
 
     // LevelCompletedNode that cycles through score values when you press 'Continue' button
     var score = 0;
