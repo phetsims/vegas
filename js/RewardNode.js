0f080ac201fa89ce4ef7c5f3ef16956594673de9
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-18 14:21:45 -0700

    Update copyright year, see https://github.com/phetsims/tasks/issues/988

--- js/RewardNode.js
+++ js/RewardNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2018, University of Colorado Boulder
+// Copyright 2014-2019, University of Colorado Boulder
 
 /**
  * Reward node that shows many nodes animating continuously, for fun!  Shown when a perfect score is achieved in a game.
