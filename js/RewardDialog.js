79a11bb9b6ed03b35798db9704d5fec8fe19133e
phet-dev <phetadmin@gmail.com>
2019-09-20 03:28:28 -0600

    update copyright dates from daily grunt work

--- js/RewardDialog.js
+++ js/RewardDialog.js
@@ -1,4 +1,4 @@
-// Copyright 2018, University of Colorado Boulder
+// Copyright 2018-2019, University of Colorado Boulder
 
 /**
  * A dialog that the client displays when the user gets a specific number of stars.
