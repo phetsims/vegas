94ded52b4553e29f3426b0a6e4ac0c3737cc4c1a
samreid <reids@colorado.edu>
2020-02-27 12:08:01 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/vegas-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import vegas from './vegas.js';
+
+const vegasStrings = getStringModule( 'VEGAS' );
+
+vegas.register( 'vegasStrings', vegasStrings );
+
+export default vegasStrings;
