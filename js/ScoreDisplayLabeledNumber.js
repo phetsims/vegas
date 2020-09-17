2b997870ddc95bc7e396f092ee195593651a0f46
pixelzoom <cmalley@pixelzoom.com>
2020-09-16 23:22:02 -0600

    convert ScoreDisplayLabeledNumber to ES6, https://github.com/phetsims/tasks/issues/1044

--- js/ScoreDisplayLabeledNumber.js
+++ js/ScoreDisplayLabeledNumber.js
@@ -8,63 +8,61 @@
  */
 
 import Utils from '../../dot/js/Utils.js';
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import StringUtils from '../../phetcommon/js/util/StringUtils.js';
 import Node from '../../scenery/js/nodes/Node.js';
 import Text from '../../scenery/js/nodes/Text.js';
 import StatusBar from './StatusBar.js';
-import vegasStrings from './vegasStrings.js';
 import vegas from './vegas.js';
+import vegasStrings from './vegasStrings.js';
 
-const patternScoreNumberString = vegasStrings.pattern.score.number;
+class ScoreDisplayLabeledNumber extends Node {
 
-/**
- * @param {Property.<number>} scoreProperty
- * @param {Object} [options]
- * @constructor
- */
-function ScoreDisplayLabeledNumber( scoreProperty, options ) {
+  /**
+   * @param {Property.<number>} scoreProperty
+   * @param {Object} [options]
+   */
+  constructor( scoreProperty, options ) {
 
-  options = merge( {
-    font: StatusBar.DEFAULT_FONT,
-    textFill: 'black',
-    scoreDecimalPlaces: 0
-  }, options );
+    options = merge( {
+      font: StatusBar.DEFAULT_FONT,
+      textFill: 'black',
+      scoreDecimalPlaces: 0
+    }, options );
 
-  const scoreDisplayText = new Text( '', {
-    font: options.font,
-    fill: options.textFill
-  } );
-
-  assert && assert( !options.children, 'ScoreDisplayNumber sets children' );
-  options.children = [ scoreDisplayText ];
-
-  // Update number displayed based on score.
-  const scorePropertyListener = function( score ) {
-    scoreDisplayText.text = StringUtils.fillIn( patternScoreNumberString, {
-      score: Utils.toFixed( score, options.scoreDecimalPlaces )
+    const scoreDisplayText = new Text( '', {
+      font: options.font,
+      fill: options.textFill
     } );
-  };
-  scoreProperty.link( scorePropertyListener );
 
-  // @private
-  this.disposeScoreDisplayLabeledNumber = function() {
-    scoreProperty.unlink( scorePropertyListener );
-  };
+    assert && assert( !options.children, 'ScoreDisplayNumber sets children' );
+    options.children = [ scoreDisplayText ];
 
-  Node.call( this, options );
+    // Update number displayed based on score.
+    const scorePropertyListener = function( score ) {
+      scoreDisplayText.text = StringUtils.fillIn( vegasStrings.pattern.score.number, {
+        score: Utils.toFixed( score, options.scoreDecimalPlaces )
+      } );
+    };
+    scoreProperty.link( scorePropertyListener );
+
+    super( options );
+
+    // @private
+    this.disposeScoreDisplayLabeledNumber = function() {
+      scoreProperty.unlink( scorePropertyListener );
+    };
+  }
+
+  /**
+   * @public
+   * @override
+   */
+  dispose() {
+    this.disposeScoreDisplayLabeledNumber();
+    super.dispose();
+  }
 }
 
 vegas.register( 'ScoreDisplayLabeledNumber', ScoreDisplayLabeledNumber );
-
-inherit( Node, ScoreDisplayLabeledNumber, {
-
-  // @public
-  dispose: function() {
-    this.disposeScoreDisplayLabeledNumber();
-    Node.prototype.dispose.call( this );
-  }
-} );
-
 export default ScoreDisplayLabeledNumber;
