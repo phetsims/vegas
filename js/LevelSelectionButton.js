9b5ebf983419d3968137234c2b55c2e1c9f3d3e7
pixelzoom <cmalley@pixelzoom.com>
2020-09-16 23:08:25 -0600

    convert LevelSelectionButton to ES6, https://github.com/phetsims/tasks/issues/1044

--- js/LevelSelectionButton.js
+++ js/LevelSelectionButton.js
@@ -12,7 +12,6 @@
  */
 
 import Dimension2 from '../../dot/js/Dimension2.js';
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import PhetFont from '../../scenery-phet/js/PhetFont.js';
 import Node from '../../scenery/js/nodes/Node.js';
@@ -34,144 +33,135 @@ const VALID_SCORE_DISPLAY_CONSTRUCTORS = [
   ScoreDisplayLabeledNumber, ScoreDisplayLabeledStars, ScoreDisplayStars, ScoreDisplayNumberAndStar
 ];
 
-/**
- * @param {Node} icon - appears on the button above the score display, scaled to fit
- * @param {Property.<number>} scoreProperty
- * @param {Object} [options]
- * @constructor
- */
-function LevelSelectionButton( icon, scoreProperty, options ) {
+class LevelSelectionButton extends RectangularPushButton {
 
-  assert && assert( icon instanceof Node );
+  /**
+   * @param {Node} icon - appears on the button above the score display, scaled to fit
+   * @param {Property.<number>} scoreProperty
+   * @param {Object} [options] - NOT propagated to super!
+   */
+  constructor( icon, scoreProperty, options ) {
 
-  options = merge( {
+    assert && assert( icon instanceof Node );
 
-    listener: null, // {function}
+    options = merge( {
 
-    // button size and appearance
-    buttonWidth: 150,
-    buttonHeight: 150,
-    cornerRadius: 10,
-    baseColor: 'rgb( 242, 255, 204 )',
-    buttonXMargin: 10,
-    buttonYMargin: 10,
+      listener: null, // {function}
 
-    // score display
-    scoreDisplayConstructor: ScoreDisplayStars,
-    scoreDisplayOptions: null, // passed to scoreDisplayConstructor
-    scoreDisplayProportion: 0.2, // percentage of the button height occupied by scoreDisplay, (0,0.5]
-    scoreDisplayMinXMargin: 10, // horizontal margin between scoreDisplay and its background
-    scoreDisplayMinYMargin: 5,  // vertical margin between scoreDisplay and its background
-    iconToScoreDisplayYSpace: 10, // vertical space between icon and score display
+      // button size and appearance
+      buttonWidth: 150,
+      buttonHeight: 150,
+      cornerRadius: 10,
+      baseColor: 'rgb( 242, 255, 204 )',
+      buttonXMargin: 10,
+      buttonYMargin: 10,
 
-    // best time (optional)
-    bestTimeProperty: null, // null if no best time || {Property.<number>} best time in seconds
-    bestTimeVisibleProperty: null, // null || Property.<boolean>} controls visibility of best time
-    bestTimeFill: 'black',
-    bestTimeFont: new PhetFont( 24 ),
-    bestTimeYSpacing: 10,  // vertical space between drop shadow and best time
+      // score display
+      scoreDisplayConstructor: ScoreDisplayStars,
+      scoreDisplayOptions: null, // passed to scoreDisplayConstructor
+      scoreDisplayProportion: 0.2, // percentage of the button height occupied by scoreDisplay, (0,0.5]
+      scoreDisplayMinXMargin: 10, // horizontal margin between scoreDisplay and its background
+      scoreDisplayMinYMargin: 5,  // vertical margin between scoreDisplay and its background
+      iconToScoreDisplayYSpace: 10, // vertical space between icon and score display
 
-    // Tandem
-    tandem: Tandem.REQUIRED
-  }, options );
+      // best time (optional)
+      bestTimeProperty: null, // null if no best time || {Property.<number>} best time in seconds
+      bestTimeVisibleProperty: null, // null || Property.<boolean>} controls visibility of best time
+      bestTimeFill: 'black',
+      bestTimeFont: new PhetFont( 24 ),
+      bestTimeYSpacing: 10,  // vertical space between drop shadow and best time
 
-  assert && assert( _.includes( VALID_SCORE_DISPLAY_CONSTRUCTORS, options.scoreDisplayConstructor,
-    'invalid scoreDisplayConstructor: ' + options.scoreDisplayConstructor ) );
-  assert && assert( options.scoreDisplayProportion > 0 && options.scoreDisplayProportion <= 0.5,
-    'scoreDisplayProportion value out of range'
-  );
+      // Tandem
+      tandem: Tandem.REQUIRED
+    }, options );
 
-  const maxContentWidth = options.buttonWidth - 2 * options.buttonXMargin;
+    assert && assert( _.includes( VALID_SCORE_DISPLAY_CONSTRUCTORS, options.scoreDisplayConstructor,
+      'invalid scoreDisplayConstructor: ' + options.scoreDisplayConstructor ) );
+    assert && assert( options.scoreDisplayProportion > 0 && options.scoreDisplayProportion <= 0.5,
+      'scoreDisplayProportion value out of range'
+    );
 
-  const scoreDisplay = new options.scoreDisplayConstructor( scoreProperty, options.scoreDisplayOptions );
+    const maxContentWidth = options.buttonWidth - 2 * options.buttonXMargin;
 
-  // Background behind scoreDisplay
-  const scoreDisplayBackgroundHeight = options.buttonHeight * options.scoreDisplayProportion;
-  const scoreDisplayBackground = new Rectangle( 0, 0, maxContentWidth, scoreDisplayBackgroundHeight, {
-    cornerRadius: options.cornerRadius,
-    fill: 'white',
-    stroke: 'black',
-    pickable: false
-  } );
+    const scoreDisplay = new options.scoreDisplayConstructor( scoreProperty, options.scoreDisplayOptions );
 
-  // constrain scoreDisplay to fit in scoreDisplayBackground
-  scoreDisplay.maxWidth = scoreDisplayBackground.width - ( 2 * options.scoreDisplayMinXMargin );
-  scoreDisplay.maxHeight = scoreDisplayBackground.height - ( 2 * options.scoreDisplayMinYMargin );
-
-  // Icon, scaled and padded to fit and to make the button size correct.
-  const iconHeight = options.buttonHeight - scoreDisplayBackground.height - 2 * options.buttonYMargin - options.iconToScoreDisplayYSpace;
-  const iconSize = new Dimension2( maxContentWidth, iconHeight );
-  const adjustedIcon = LevelSelectionButton.createSizedImageNode( icon, iconSize );
-  adjustedIcon.centerX = scoreDisplayBackground.centerX;
-  adjustedIcon.bottom = scoreDisplayBackground.top - options.iconToScoreDisplayYSpace;
-
-  // Keep scoreDisplay centered in its background when its bounds change
-  const scoreDisplayUpdateLayout = function() {
-    scoreDisplay.center = scoreDisplayBackground.center;
-  };
-  scoreDisplay.boundsProperty.lazyLink( scoreDisplayUpdateLayout );
-  scoreDisplayUpdateLayout();
-
-  RectangularPushButton.call( this, {
-    content: new Node( { children: [ adjustedIcon, scoreDisplayBackground, scoreDisplay ] } ),
-    xMargin: options.buttonXMargin,
-    yMargin: options.buttonYMargin,
-    baseColor: options.baseColor,
-    cornerRadius: options.cornerRadius,
-    listener: options.listener
-  } );
-
-  // Best time decoration (optional), centered below the button, does not move when button is pressed
-  if ( options.bestTimeProperty ) {
-
-    const bestTimeNode = new Text( '', {
-      font: options.bestTimeFont,
-      fill: options.bestTimeFill,
-      maxWidth: this.width // constrain to width of the push button
+    // Background behind scoreDisplay
+    const scoreDisplayBackgroundHeight = options.buttonHeight * options.scoreDisplayProportion;
+    const scoreDisplayBackground = new Rectangle( 0, 0, maxContentWidth, scoreDisplayBackgroundHeight, {
+      cornerRadius: options.cornerRadius,
+      fill: 'white',
+      stroke: 'black',
+      pickable: false
     } );
-    const centerX = this.centerX;
-    bestTimeNode.top = this.bottom + options.bestTimeYSpacing;
-    this.addChild( bestTimeNode );
 
-    var bestTimeListener = function( bestTime ) {
-      bestTimeNode.text = ( bestTime ? GameTimer.formatTime( bestTime ) : '' );
-      bestTimeNode.centerX = centerX;
+    // constrain scoreDisplay to fit in scoreDisplayBackground
+    scoreDisplay.maxWidth = scoreDisplayBackground.width - ( 2 * options.scoreDisplayMinXMargin );
+    scoreDisplay.maxHeight = scoreDisplayBackground.height - ( 2 * options.scoreDisplayMinYMargin );
+
+    // Icon, scaled and padded to fit and to make the button size correct.
+    const iconHeight = options.buttonHeight - scoreDisplayBackground.height - 2 * options.buttonYMargin - options.iconToScoreDisplayYSpace;
+    const iconSize = new Dimension2( maxContentWidth, iconHeight );
+    const adjustedIcon = LevelSelectionButton.createSizedImageNode( icon, iconSize );
+    adjustedIcon.centerX = scoreDisplayBackground.centerX;
+    adjustedIcon.bottom = scoreDisplayBackground.top - options.iconToScoreDisplayYSpace;
+
+    // Keep scoreDisplay centered in its background when its bounds change
+    const scoreDisplayUpdateLayout = function() {
+      scoreDisplay.center = scoreDisplayBackground.center;
     };
-    options.bestTimeProperty.link( bestTimeListener );
+    scoreDisplay.boundsProperty.lazyLink( scoreDisplayUpdateLayout );
+    scoreDisplayUpdateLayout();
 
-    if ( options.bestTimeVisibleProperty ) {
-      var bestTimeVisibleListener = function( visible ) {
-        bestTimeNode.visible = visible;
+    super( {
+      content: new Node( { children: [ adjustedIcon, scoreDisplayBackground, scoreDisplay ] } ),
+      xMargin: options.buttonXMargin,
+      yMargin: options.buttonYMargin,
+      baseColor: options.baseColor,
+      cornerRadius: options.cornerRadius,
+      listener: options.listener
+    } );
+
+    // Best time decoration (optional), centered below the button, does not move when button is pressed
+    if ( options.bestTimeProperty ) {
+
+      const bestTimeNode = new Text( '', {
+        font: options.bestTimeFont,
+        fill: options.bestTimeFill,
+        maxWidth: this.width // constrain to width of the push button
+      } );
+      const centerX = this.centerX;
+      bestTimeNode.top = this.bottom + options.bestTimeYSpacing;
+      this.addChild( bestTimeNode );
+
+      var bestTimeListener = function( bestTime ) {
+        bestTimeNode.text = ( bestTime ? GameTimer.formatTime( bestTime ) : '' );
+        bestTimeNode.centerX = centerX;
       };
-      options.bestTimeVisibleProperty.link( bestTimeVisibleListener );
+      options.bestTimeProperty.link( bestTimeListener );
+
+      if ( options.bestTimeVisibleProperty ) {
+        var bestTimeVisibleListener = function( visible ) {
+          bestTimeNode.visible = visible;
+        };
+        options.bestTimeVisibleProperty.link( bestTimeVisibleListener );
+      }
     }
+
+    // @private
+    this.disposeLevelSelectionButton = function() {
+
+      scoreDisplay.dispose();
+
+      if ( bestTimeListener && options.bestTimeProperty.hasListener( bestTimeListener ) ) {
+        options.bestTimeProperty.unlink( bestTimeListener );
+      }
+
+      if ( bestTimeVisibleListener && options.bestTimeVisibleProperty.hasListener( bestTimeVisibleListener ) ) {
+        options.bestTimeVisibleProperty.unlink( bestTimeVisibleListener );
+      }
+    };
   }
 
-  // @private
-  this.disposeLevelSelectionButton = function() {
-
-    scoreDisplay.dispose();
-
-    if ( bestTimeListener && options.bestTimeProperty.hasListener( bestTimeListener ) ) {
-      options.bestTimeProperty.unlink( bestTimeListener );
-    }
-
-    if ( bestTimeVisibleListener && options.bestTimeVisibleProperty.hasListener( bestTimeVisibleListener ) ) {
-      options.bestTimeVisibleProperty.unlink( bestTimeVisibleListener );
-    }
-  };
-}
-
-vegas.register( 'LevelSelectionButton', LevelSelectionButton );
-
-inherit( RectangularPushButton, LevelSelectionButton, {
-
-  // @public
-  dispose: function() {
-    this.disposeLevelSelectionButton();
-    RectangularPushButton.prototype.dispose.call( this );
-  }
-}, {
   /**
    * Creates a new icon with the same dimensions as the specified icon. The new icon will be scaled to fit,
    * and a background with the specified size may be added to ensure that the bounds of the returned node are correct.
@@ -181,19 +171,30 @@ inherit( RectangularPushButton, LevelSelectionButton, {
    * @param {Dimension2} size
    * @returns {Node}
    */
-  createSizedImageNode: function( icon, size ) {
+  static createSizedImageNode( icon, size ) {
     icon.scale( Math.min( size.width / icon.bounds.width, size.height / icon.bounds.height ) );
     if ( Math.abs( icon.bounds.width - size.width ) < SCALING_TOLERANCE &&
          Math.abs( icon.bounds.height - size.height ) < SCALING_TOLERANCE ) {
       // The aspect ratio of the icon matched that of the specified size, so no padding is necessary.
       return icon;
     }
+
     // else padding is needed in either the horizontal or vertical direction.
     const background = Rectangle.dimension( size );
     icon.center = background.center;
     background.addChild( icon );
     return background;
   }
-} );
 
+  /**
+   * @public
+   * @override
+   */
+  dispose() {
+    this.disposeLevelSelectionButton();
+    super.dispose();
+  }
+}
+
+vegas.register( 'LevelSelectionButton', LevelSelectionButton );
 export default LevelSelectionButton;
