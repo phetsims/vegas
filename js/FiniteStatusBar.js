ead11d0a3534544fb2646d38929d4e8fcc0eac25
pixelzoom <cmalley@pixelzoom.com>
2020-10-13 15:19:00 -0600

    convert to ES6 class, https://github.com/phetsims/vegas/issues/83

--- js/FiniteStatusBar.js
+++ js/FiniteStatusBar.js
@@ -7,12 +7,10 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import StringUtils from '../../phetcommon/js/util/StringUtils.js';
 import PhetColorScheme from '../../scenery-phet/js/PhetColorScheme.js';
 import HBox from '../../scenery/js/nodes/HBox.js';
-import Node from '../../scenery/js/nodes/Node.js';
 import Rectangle from '../../scenery/js/nodes/Rectangle.js';
 import Text from '../../scenery/js/nodes/Text.js';
 import TextPushButton from '../../sun/js/buttons/TextPushButton.js';
@@ -24,225 +22,221 @@ import StatusBar from './StatusBar.js';
 import vegas from './vegas.js';
 import vegasStrings from './vegasStrings.js';
 
-const labelLevelString = vegasStrings.label.level;
-const pattern0Challenge1MaxString = vegasStrings.pattern[ '0challenge' ][ '1max' ];
-const startOverString = vegasStrings.startOver;
-
-// constants
+// Valid values for scoreDisplayConstructor. These are the types that are relevant for this status bar.
+// All constructors must have the same signature!
 const VALID_SCORE_DISPLAY_CONSTRUCTORS = [
-  // Types that are relevant for this status bar. All constructors must have the same signature!
   ScoreDisplayLabeledNumber, ScoreDisplayLabeledStars
 ];
 
-/**
- * @param {Bounds2} layoutBounds
- * @param {Property.<Bounds2>} visibleBoundsProperty
- * @param {Property.<number>} scoreProperty
- * @param {Object} [options]
- * @constructor
- */
-function FiniteStatusBar( layoutBounds, visibleBoundsProperty, scoreProperty, options ) {
+class FiniteStatusBar extends StatusBar {
 
-  options = merge( {
+  /**
+   * @param {Bounds2} layoutBounds
+   * @param {Property.<Bounds2>} visibleBoundsProperty
+   * @param {Property.<number>} scoreProperty
+   * @param {Object} [options]
+   */
+  constructor( layoutBounds, visibleBoundsProperty, scoreProperty, options ) {
 
-    // optional Properties
-    challengeIndexProperty: null, // {Property.<number>|null}
-    numberOfChallengesProperty: null, // {Property.<number>|null}
-    levelProperty: null, // {Property.<number>|null}
-    elapsedTimeProperty: null, // {Property.<number>|null} 
-    timerEnabledProperty: null, // {Property.<boolean>|null} 
+    options = merge( {
 
-    // things that can be hidden
-    levelVisible: true,
-    challengeNumberVisible: true,
+      // optional Properties
+      challengeIndexProperty: null, // {Property.<number>|null}
+      numberOfChallengesProperty: null, // {Property.<number>|null}
+      levelProperty: null, // {Property.<number>|null}
+      elapsedTimeProperty: null, // {Property.<number>|null}
+      timerEnabledProperty: null, // {Property.<boolean>|null}
 
-    // all text
-    font: StatusBar.DEFAULT_FONT,
-    textFill: 'black',
+      // things that can be hidden
+      levelVisible: true,
+      challengeNumberVisible: true,
 
-    // score display
-    scoreDisplayConstructor: ScoreDisplayLabeledNumber,
-    scoreDisplayOptions: null, // passed to scoreDisplayConstructor
+      // all text
+      font: StatusBar.DEFAULT_FONT,
+      textFill: 'black',
 
-    // nested options for 'Start Over' button, filled in below
-    startOverButtonOptions: null,
-    startOverButtonText: startOverString,
+      // score display
+      scoreDisplayConstructor: ScoreDisplayLabeledNumber,
+      scoreDisplayOptions: null, // passed to scoreDisplayConstructor
 
-    // Timer
-    clockIconRadius: 15,
+      // nested options for 'Start Over' button, filled in below
+      startOverButtonOptions: null,
+      startOverButtonText: vegasStrings.startOver,
 
-    // spacing and margin for things in the bar
-    xSpacing: 50,
-    xMargin: 20,
-    yMargin: 10,
+      // Timer
+      clockIconRadius: 15,
 
-    levelTextOptions: null, // passed to the "Level N" text
-    challengeTextOptions: null, // passed to the "Challenge N of M" text
+      // spacing and margin for things in the bar
+      xSpacing: 50,
+      xMargin: 20,
+      yMargin: 10,
 
-    // phet-io
-    tandem: Tandem.REQUIRED
-  }, options );
+      levelTextOptions: null, // passed to the "Level N" text
+      challengeTextOptions: null, // passed to the "Challenge N of M" text
 
-  // nested options for score display
-  options.scoreDisplayOptions = merge( {
-    font: options.font,
-    textFill: options.textFill
-  }, options.scoreDisplayOptions );
+      // phet-io
+      tandem: Tandem.REQUIRED
+    }, options );
 
-  // nested options for 'Start Over' button
-  options.startOverButtonOptions = merge( {
-    font: options.font,
-    textFill: options.textFill,
-    baseColor: PhetColorScheme.BUTTON_YELLOW,
-    xMargin: 10,
-    yMargin: 8,
-    listener: () => {},
-    tandem: options.tandem.createTandem( 'startOverButton' ),
-    maxWidth: 0.2 * ( layoutBounds.width - ( 2 * options.xMargin ) ) // use 20% of available width
-  }, options.startOverButtonOptions );
-
-  assert && assert( _.includes( VALID_SCORE_DISPLAY_CONSTRUCTORS, options.scoreDisplayConstructor,
-    'invalid scoreDisplayConstructor: ' + options.scoreDisplayConstructor ) );
-  assert && assert( ( options.challengeIndexProperty && options.numberOfChallengesProperty ) ||
-                    ( !options.challengeIndexProperty && !options.numberOfChallengesProperty ),
-    'challengeIndexProperty and numberOfChallengesProperty are both or neither' );
-
-  // nested options for 'Level N' text
-  options.levelTextOptions = merge( {
-    fill: options.textFill,
-    font: options.font
-  }, options.levelTextOptions );
-
-  // nested options for 'Challenge N of M' text
-  options.challengeTextOptions = merge( {
-    fill: options.textFill,
-    font: options.font
-  }, options.challengeTextOptions );
-
-  // the rectangular bar, size will be set by visibleBoundsListener
-  const barNode = new Rectangle( {
-    fill: options.barFill,
-    stroke: options.barStroke
-  } );
-
-  // Nodes on the left end of the bar
-  const leftChildren = [];
-
-  // Level N
-  if ( options.levelProperty && options.levelVisible ) {
-    const levelText = new Text( '', merge( {
-      tandem: options.tandem.createTandem( 'levelText' )
-    }, options.levelTextOptions ) );
-    leftChildren.push( levelText );
-
-    var levelListener = level => {
-      levelText.text = StringUtils.format( labelLevelString, level );
-    };
-    options.levelProperty.link( levelListener );
-  }
-
-  // Challenge N of M
-  if ( options.challengeIndexProperty && options.numberOfChallengesProperty ) {
-    const challengeNumberText = new Text( '', merge( {
-      tandem: options.tandem.createTandem( 'challengeNumberText' )
-    }, options.challengeTextOptions ) );
-    leftChildren.push( challengeNumberText );
-
-    var updateChallengeString = () => {
-      challengeNumberText.text = StringUtils.format( pattern0Challenge1MaxString,
-        options.challengeIndexProperty.get() + 1, options.numberOfChallengesProperty.get() );
-    };
-    options.challengeIndexProperty.link( updateChallengeString );
-    options.numberOfChallengesProperty.link( updateChallengeString );
-  }
-
-  // Score
-  const scoreDisplay = new options.scoreDisplayConstructor( scoreProperty, options.scoreDisplayOptions );
-  leftChildren.push( scoreDisplay );
-
-  // Timer
-  if ( options.elapsedTimeProperty && options.timerEnabledProperty ) {
-
-    var elapsedTimeNode = new ElapsedTimeNode( options.elapsedTimeProperty, {
-      clockRadius: options.clockRadius,
+    // nested options for score display
+    options.scoreDisplayOptions = merge( {
       font: options.font,
       textFill: options.textFill
+    }, options.scoreDisplayOptions );
+
+    // nested options for 'Start Over' button
+    options.startOverButtonOptions = merge( {
+      font: options.font,
+      textFill: options.textFill,
+      baseColor: PhetColorScheme.BUTTON_YELLOW,
+      xMargin: 10,
+      yMargin: 8,
+      listener: () => {},
+      tandem: options.tandem.createTandem( 'startOverButton' ),
+      maxWidth: 0.2 * ( layoutBounds.width - ( 2 * options.xMargin ) ) // use 20% of available width
+    }, options.startOverButtonOptions );
+
+    assert && assert( _.includes( VALID_SCORE_DISPLAY_CONSTRUCTORS, options.scoreDisplayConstructor,
+      'invalid scoreDisplayConstructor: ' + options.scoreDisplayConstructor ) );
+    assert && assert( ( options.challengeIndexProperty && options.numberOfChallengesProperty ) ||
+                      ( !options.challengeIndexProperty && !options.numberOfChallengesProperty ),
+      'challengeIndexProperty and numberOfChallengesProperty are both or neither' );
+
+    // nested options for 'Level N' text
+    options.levelTextOptions = merge( {
+      fill: options.textFill,
+      font: options.font
+    }, options.levelTextOptions );
+
+    // nested options for 'Challenge N of M' text
+    options.challengeTextOptions = merge( {
+      fill: options.textFill,
+      font: options.font
+    }, options.challengeTextOptions );
+
+    // the rectangular bar, size will be set by visibleBoundsListener
+    const barNode = new Rectangle( {
+      fill: options.barFill,
+      stroke: options.barStroke
     } );
-    leftChildren.push( elapsedTimeNode );
 
-    var timerEnabledListener = timerEnabled => {
-      elapsedTimeNode.visible = ( options.timerEnabledProperty && timerEnabled );
-    };
-    options.timerEnabledProperty && options.timerEnabledProperty.link( timerEnabledListener );
-  }
+    // Nodes on the left end of the bar
+    const leftChildren = [];
 
-  // Start Over button
-  const startOverButton = new TextPushButton( options.startOverButtonText, options.startOverButtonOptions );
+    // Level N
+    if ( options.levelProperty && options.levelVisible ) {
 
-  // Nodes on the left end of the bar
-  const leftNodes = new HBox( {
+      const levelText = new Text( '', merge( {
+        tandem: options.tandem.createTandem( 'levelText' )
+      }, options.levelTextOptions ) );
+      leftChildren.push( levelText );
 
-    // Because elapsedTimeNode needs to be considered regardless of whether it's visible,
-    // see https://github.com/phetsims/vegas/issues/80
-    excludeInvisibleChildrenFromBounds: false,
-    resize: false,
-    spacing: options.xSpacing,
-    children: leftChildren,
-    maxWidth: ( layoutBounds.width - ( 2 * options.xMargin ) - startOverButton.width - options.xSpacing )
-  } );
-
-  assert && assert( !options.children, 'FiniteStatusBar sets children' );
-  options.children = [ barNode, leftNodes, startOverButton ];
-
-  assert && assert( options.barHeight === undefined, 'InfiniteStatusBar sets barHeight' );
-  options.barHeight = Math.max( leftNodes.height, scoreDisplay.height ) + ( 2 * options.yMargin );
-
-  StatusBar.call( this, layoutBounds, visibleBoundsProperty, options );
-
-  // Position components on the bar.
-  this.positioningBoundsProperty.link( positioningBounds => {
-    leftNodes.left = positioningBounds.left;
-    leftNodes.centerY = positioningBounds.centerY;
-    startOverButton.right = positioningBounds.right;
-    startOverButton.centerY = positioningBounds.centerY;
-  } );
-
-  // @private
-  this.disposeFiniteStatusBar = () => {
-
-    scoreDisplay.dispose();
-    elapsedTimeNode && elapsedTimeNode.dispose();
-
-    if ( options.levelProperty && options.levelProperty.hasListener( levelListener ) ) {
-      options.levelProperty.unlink( levelListener );
+      var levelListener = level => {
+        levelText.text = StringUtils.format( vegasStrings.label.level, level );
+      };
+      options.levelProperty.link( levelListener );
     }
 
-    if ( options.challengeIndexProperty && options.challengeIndexProperty.hasListener( updateChallengeString ) ) {
-      options.challengeIndexProperty.unlink( updateChallengeString );
-    }
+    // Challenge N of M
+    if ( options.challengeIndexProperty && options.numberOfChallengesProperty ) {
 
-    if ( options.numberOfChallengesProperty && options.numberOfChallengesProperty.hasListener( updateChallengeString ) ) {
+      const challengeNumberText = new Text( '', merge( {
+        tandem: options.tandem.createTandem( 'challengeNumberText' )
+      }, options.challengeTextOptions ) );
+      leftChildren.push( challengeNumberText );
+
+      var updateChallengeString = () => {
+        challengeNumberText.text = StringUtils.format( vegasStrings.pattern[ '0challenge' ][ '1max' ],
+          options.challengeIndexProperty.get() + 1, options.numberOfChallengesProperty.get() );
+      };
+      options.challengeIndexProperty.link( updateChallengeString );
       options.numberOfChallengesProperty.link( updateChallengeString );
     }
 
-    if ( options.timerEnabledProperty && options.timerEnabledProperty.hasListener( timerEnabledListener ) ) {
-      options.timerEnabledProperty.unlink( timerEnabledListener );
+    // Score
+    const scoreDisplay = new options.scoreDisplayConstructor( scoreProperty, options.scoreDisplayOptions );
+    leftChildren.push( scoreDisplay );
+
+    // Timer
+    if ( options.elapsedTimeProperty && options.timerEnabledProperty ) {
+
+      var elapsedTimeNode = new ElapsedTimeNode( options.elapsedTimeProperty, {
+        clockRadius: options.clockRadius,
+        font: options.font,
+        textFill: options.textFill
+      } );
+      leftChildren.push( elapsedTimeNode );
+
+      var timerEnabledListener = timerEnabled => {
+        elapsedTimeNode.visible = ( options.timerEnabledProperty && timerEnabled );
+      };
+      options.timerEnabledProperty && options.timerEnabledProperty.link( timerEnabledListener );
     }
-  };
-}
 
-vegas.register( 'FiniteStatusBar', FiniteStatusBar );
+    // Start Over button
+    const startOverButton = new TextPushButton( options.startOverButtonText, options.startOverButtonOptions );
 
-inherit( Node, FiniteStatusBar, {
+    // Nodes on the left end of the bar
+    const leftNodes = new HBox( {
+
+      // Because elapsedTimeNode needs to be considered regardless of whether it's visible,
+      // see https://github.com/phetsims/vegas/issues/80
+      excludeInvisibleChildrenFromBounds: false,
+      resize: false,
+      spacing: options.xSpacing,
+      children: leftChildren,
+      maxWidth: ( layoutBounds.width - ( 2 * options.xMargin ) - startOverButton.width - options.xSpacing )
+    } );
+
+    assert && assert( !options.children, 'FiniteStatusBar sets children' );
+    options.children = [ barNode, leftNodes, startOverButton ];
+
+    assert && assert( options.barHeight === undefined, 'FiniteStatusBar sets barHeight' );
+    options.barHeight = Math.max( leftNodes.height, scoreDisplay.height ) + ( 2 * options.yMargin );
+
+    super( layoutBounds, visibleBoundsProperty, options );
+
+    // Position components on the bar.
+    this.positioningBoundsProperty.link( positioningBounds => {
+      leftNodes.left = positioningBounds.left;
+      leftNodes.centerY = positioningBounds.centerY;
+      startOverButton.right = positioningBounds.right;
+      startOverButton.centerY = positioningBounds.centerY;
+    } );
+
+    // @private
+    this.disposeFiniteStatusBar = () => {
+
+      scoreDisplay.dispose();
+      elapsedTimeNode && elapsedTimeNode.dispose();
+
+      if ( options.levelProperty && options.levelProperty.hasListener( levelListener ) ) {
+        options.levelProperty.unlink( levelListener );
+      }
+
+      if ( options.challengeIndexProperty && options.challengeIndexProperty.hasListener( updateChallengeString ) ) {
+        options.challengeIndexProperty.unlink( updateChallengeString );
+      }
+
+      if ( options.numberOfChallengesProperty && options.numberOfChallengesProperty.hasListener( updateChallengeString ) ) {
+        options.numberOfChallengesProperty.link( updateChallengeString );
+      }
+
+      if ( options.timerEnabledProperty && options.timerEnabledProperty.hasListener( timerEnabledListener ) ) {
+        options.timerEnabledProperty.unlink( timerEnabledListener );
+      }
+    };
+  }
 
   /**
    * @public
    * @override
    */
-  dispose: function() {
+  dispose() {
     this.disposeFiniteStatusBar();
-    Node.prototype.dispose.call( this );
+    super.dispose();
   }
-} );
+}
 
+vegas.register( 'FiniteStatusBar', FiniteStatusBar );
 export default FiniteStatusBar;
--- js/InfiniteStatusBar.js
+++ js/InfiniteStatusBar.js
@@ -8,103 +8,99 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import BackButton from '../../scenery-phet/js/buttons/BackButton.js';
 import HBox from '../../scenery/js/nodes/HBox.js';
-import Node from '../../scenery/js/nodes/Node.js';
 import ScoreDisplayLabeledNumber from './ScoreDisplayLabeledNumber.js';
 import ScoreDisplayNumberAndStar from './ScoreDisplayNumberAndStar.js';
 import StatusBar from './StatusBar.js';
 import vegas from './vegas.js';
 
-// constants
+// Valid values for scoreDisplayConstructor. These are the types that are relevant for this status bar.
+// All constructors must have the same signature!
 const VALID_SCORE_DISPLAY_CONSTRUCTORS = [
-  // Types that are relevant for this status bar. All constructors must have the same signature!
   ScoreDisplayLabeledNumber, ScoreDisplayNumberAndStar
 ];
 
-/**
- * @param {Bounds2} layoutBounds - static 'safe' bounds of the parent ScreenView
- * @param {Property.<Bounds2>} visibleBoundsProperty - dynamic bounds of the browser window
- * @param {Node} messageNode - to the right of the back button, typically Text
- * @param {Property.<number>} scoreProperty
- * @param {Object} [options]
- * @constructor
- */
-function InfiniteStatusBar( layoutBounds, visibleBoundsProperty, messageNode, scoreProperty, options ) {
+class InfiniteStatusBar extends StatusBar {
 
-  options = merge( {
-    backButtonListener: null,
-    xMargin: 20,
-    yMargin: 10,
-    spacing: 10,
+  /**
+   * @param {Bounds2} layoutBounds - static 'safe' bounds of the parent ScreenView
+   * @param {Property.<Bounds2>} visibleBoundsProperty - dynamic bounds of the browser window
+   * @param {Node} messageNode - to the right of the back button, typically Text
+   * @param {Property.<number>} scoreProperty
+   * @param {Object} [options]
+   */
+  constructor( layoutBounds, visibleBoundsProperty, messageNode, scoreProperty, options ) {
 
-    // score display
-    scoreDisplayConstructor: ScoreDisplayNumberAndStar,
-    scoreDisplayOptions: null // passed to scoreDisplayConstructor
-  }, options );
+    options = merge( {
+      backButtonListener: null,
+      xMargin: 20,
+      yMargin: 10,
+      spacing: 10,
 
-  assert && assert( _.includes( VALID_SCORE_DISPLAY_CONSTRUCTORS, options.scoreDisplayConstructor,
-    'invalid scoreDisplayConstructor: ' + options.scoreDisplayConstructor ) );
+      // score display
+      scoreDisplayConstructor: ScoreDisplayNumberAndStar,
+      scoreDisplayOptions: null // passed to scoreDisplayConstructor
+    }, options );
 
-  // button that typically takes us back to the level-selection UI
-  const backButton = new BackButton( {
-    listener: options.backButtonListener,
-    xMargin: 8,
-    yMargin: 10
-  } );
+    assert && assert( _.includes( VALID_SCORE_DISPLAY_CONSTRUCTORS, options.scoreDisplayConstructor,
+      'invalid scoreDisplayConstructor: ' + options.scoreDisplayConstructor ) );
 
-  // Nodes on the left end of the bar
-  const leftNodes = new HBox( {
-    spacing: options.spacing,
-    align: 'center',
-    children: [ backButton, messageNode ],
-    maxWidth: 0.7 * layoutBounds.width
-  } );
+    // button that typically takes us back to the level-selection UI
+    const backButton = new BackButton( {
+      listener: options.backButtonListener,
+      xMargin: 8,
+      yMargin: 10
+    } );
 
-  const scoreDisplay = new options.scoreDisplayConstructor( scoreProperty,
-    merge( { maxWidth: 0.2 * layoutBounds.width }, options.scoreDisplayOptions ) );
+    // Nodes on the left end of the bar
+    const leftNodes = new HBox( {
+      spacing: options.spacing,
+      align: 'center',
+      children: [ backButton, messageNode ],
+      maxWidth: 0.7 * layoutBounds.width
+    } );
 
-  assert && assert( !options.children, 'InfiniteStatusBar sets children' );
-  options.children = [ leftNodes, scoreDisplay ];
+    const scoreDisplay = new options.scoreDisplayConstructor( scoreProperty,
+      merge( { maxWidth: 0.2 * layoutBounds.width }, options.scoreDisplayOptions ) );
 
-  assert && assert( options.barHeight === undefined, 'InfiniteStatusBar sets barHeight' );
-  options.barHeight = Math.max( leftNodes.height, scoreDisplay.height ) + ( 2 * options.yMargin );
+    assert && assert( !options.children, 'InfiniteStatusBar sets children' );
+    options.children = [ leftNodes, scoreDisplay ];
 
-  StatusBar.call( this, layoutBounds, visibleBoundsProperty, options );
+    assert && assert( options.barHeight === undefined, 'InfiniteStatusBar sets barHeight' );
+    options.barHeight = Math.max( leftNodes.height, scoreDisplay.height ) + ( 2 * options.yMargin );
 
-  // Position components on the bar.
-  this.positioningBoundsProperty.link( positioningBounds => {
-    leftNodes.left = positioningBounds.left;
-    leftNodes.centerY = positioningBounds.centerY;
-    scoreDisplay.right = positioningBounds.right;
-    scoreDisplay.centerY = positioningBounds.centerY;
-  } );
+    super( layoutBounds, visibleBoundsProperty, options );
 
-  // Keep the score right justified.
-  scoreDisplay.localBoundsProperty.link( () => {
-    scoreDisplay.right = this.positioningBoundsProperty.value.right;
-  } );
+    // Position components on the bar.
+    this.positioningBoundsProperty.link( positioningBounds => {
+      leftNodes.left = positioningBounds.left;
+      leftNodes.centerY = positioningBounds.centerY;
+      scoreDisplay.right = positioningBounds.right;
+      scoreDisplay.centerY = positioningBounds.centerY;
+    } );
 
-  // @private
-  this.disposeInfiniteStatusBar = () => {
-    scoreDisplay.dispose();
-  };
-}
+    // Keep the score right justified.
+    scoreDisplay.localBoundsProperty.link( () => {
+      scoreDisplay.right = this.positioningBoundsProperty.value.right;
+    } );
 
-vegas.register( 'InfiniteStatusBar', InfiniteStatusBar );
-
-inherit( StatusBar, InfiniteStatusBar, {
+    // @private
+    this.disposeInfiniteStatusBar = () => {
+      scoreDisplay.dispose();
+    };
+  }
 
   /**
    * @public
    * @override
    */
-  dispose: function() {
+  dispose() {
     this.disposeInfiniteStatusBar();
-    Node.prototype.dispose.call( this );
+    super.dispose();
   }
-} );
+}
 
+vegas.register( 'InfiniteStatusBar', InfiniteStatusBar );
 export default InfiniteStatusBar;
