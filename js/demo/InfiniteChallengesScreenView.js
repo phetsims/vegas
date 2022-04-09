b9508af28038975facd091fdacffbc34807adf94
phet-dev <phetadmin@gmail.com>
2022-04-09 03:43:41 -0600

    update copyright dates from daily grunt work

--- js/demo/InfiniteChallengesScreenView.js
+++ js/demo/InfiniteChallengesScreenView.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2021, University of Colorado Boulder
+// Copyright 2014-2022, University of Colorado Boulder
 
 /**
  * Demonstrates the challenge UI for a game that has an infinite number of challenges per level.
