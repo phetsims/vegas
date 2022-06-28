3cef3e4f777a372af113a5dd873c957e5d8a8b15
phet-dev <phetadmin@gmail.com>
2022-06-28 04:02:27 -0600

    update copyright dates from daily grunt work

--- js/GameInfoDialog.ts
+++ js/GameInfoDialog.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2022, University of Colorado Boulder
+// Copyright 2022, University of Colorado Boulder
 
 /**
  * GameInfoDialog shows descriptions for the levels of a game.  Each description is on a separate line.
