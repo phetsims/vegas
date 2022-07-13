9588d6a60af51a993afee3a2c512178f175b1145
pixelzoom <cmalley@pixelzoom.com>
2022-07-13 11:59:25 -0600

    add time controls to demoRewardNode.ts

--- js/demo/components/demoRewardNode.ts
+++ js/demo/components/demoRewardNode.ts
@@ -7,16 +7,35 @@
  */
 
 import Bounds2 from '../../../../dot/js/Bounds2.js';
+import { Node } from '../../../../scenery/js/imports.js';
 import RewardNode from '../../RewardNode.js';
 import stepTimer from '../../../../axon/js/stepTimer.js';
+import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
+import TimeControlNode from '../../../../scenery-phet/js/TimeControlNode.js';
 
 export default function demoRewardNode( layoutBounds: Bounds2 ) {
 
   const rewardNode = new RewardNode();
 
-  stepTimer.addListener( dt => {
-    rewardNode.step( dt );
+  const isPlayingProperty = new BooleanProperty( true );
+
+  const timeControls = new TimeControlNode( isPlayingProperty, {
+    playPauseStepButtonOptions: {
+      stepForwardButtonOptions: {
+        listener: () => rewardNode.step( 0.1 )
+      }
+    },
+    centerX: layoutBounds.centerX,
+    bottom: layoutBounds.bottom - 20
   } );
 
-  return rewardNode;
+  stepTimer.addListener( dt => {
+    if ( isPlayingProperty.value ) {
+      rewardNode.step( dt );
+    }
+  } );
+
+  return new Node( {
+    children: [ rewardNode, timeControls ]
+  } );
 }
