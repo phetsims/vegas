e2eca833ef0ee2863130c2aa3bb0aeb9df77af49
phet-dev <phetadmin@gmail.com>
2023-06-27 03:38:08 -0600

    update copyright dates from daily grunt work

--- js/InfiniteStatusBar.ts
+++ js/InfiniteStatusBar.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2022, University of Colorado Boulder
+// Copyright 2018-2023, University of Colorado Boulder
 
 /**
  * InfiniteStatusBar is the status bar for games that have an infinite (open-ended) number of challenges per level.
