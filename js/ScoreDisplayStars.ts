bda60624e913f2ccd0c6ebdc377ba943bd986f7b
phet-dev <phetadmin@gmail.com>
2023-12-07 03:21:22 -0700

    update copyright dates from daily grunt work

--- js/ScoreDisplayStars.ts
+++ js/ScoreDisplayStars.ts
@@ -1,4 +1,4 @@
-// Copyright 2013-2022, University of Colorado Boulder
+// Copyright 2013-2023, University of Colorado Boulder
 
 /**
  * Display a score as '* * * *', where '*' are stars, which may be fully or partially filled in.
