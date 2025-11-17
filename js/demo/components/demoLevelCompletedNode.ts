2d9bc6633dd97293594f0d0d6f21df2c8fbce5a0
Jesse <jesse.greenberg@colorado.edu>
2025-11-17 17:39:21 -0500

    update demo to show context response for LevelCompletedNode, see https://github.com/phetsims/vegas/issues/147

--- js/demo/components/demoLevelCompletedNode.ts
+++ js/demo/components/demoLevelCompletedNode.ts
@@ -6,20 +6,40 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
 import Bounds2 from '../../../../dot/js/Bounds2.js';
+import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
+import VBox from '../../../../scenery/js/layout/nodes/VBox.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
+import TextPushButton from '../../../../sun/js/buttons/TextPushButton.js';
 import LevelCompletedNode from '../../LevelCompletedNode.js';
 
 export default function demoLevelCompletedNode( layoutBounds: Bounds2 ): Node {
 
-  const levelCompletedNode = new LevelCompletedNode( 1, 10, 10, 5, true, 2000, null, true, _.noop, {
-    contentMaxWidth: 500
+  const visibleProperty = new BooleanProperty( false );
+  const levelCompletedNode = new LevelCompletedNode( 1, 10, 10, 5, true, 2000, null, true,
+    () => visibleProperty.toggle(), {
+    contentMaxWidth: 500,
+    visibleProperty: visibleProperty
+  } );
+
+  const completeLevelButton = new TextPushButton( 'Complete Level', {
+    font: new PhetFont( 32 ),
+    listener: () => {
+      visibleProperty.toggle();
+    }
+  } );
+
+  const container = new VBox( {
+    children: [ completeLevelButton, levelCompletedNode ],
+    align: 'center',
+    excludeInvisibleChildrenFromBounds: false
   } );
 
   // LevelCompletedNode has dynamic strings, so requires dynamic layout.
   levelCompletedNode.boundsProperty.link( () => {
-    levelCompletedNode.center = layoutBounds.center;
+    container.centerTop = layoutBounds.centerTop.plusXY( 0, 60 );
   } );
 
-  return levelCompletedNode;
+  return container;
 }
